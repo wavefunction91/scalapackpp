535679fc9b99b759e5ad444c37e7edaab08cefd2
David Williams-Young <dbwy@lbl.gov>
2021-01-29 16:50:10 -0800

    [CMAKE] Typo

--- tests/cmake/discovery/scalapackpp_linker_test.cxx
+++ tests/cmake/discovery/scalapackpp_linker_test.cxx
@@ -3,7 +3,7 @@
 int main() {
   MPI_Init( NULL, NULL );
   {
-    std::shared_ptr<const Grid> grid = std::make_shared<const Grid>(blacspp::Grid::square_grid( MPI_COMM_WORLD ));
+    auto grid = std::make_shared<const blacspp::Grid>(blacspp::Grid::square_grid( MPI_COMM_WORLD ));
     scalapackpp::BlockCyclicDist2D dist( grid, 2, 2 );
   }
   MPI_Finalize();
