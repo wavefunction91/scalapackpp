b9cd2c3b7334174d018948a29df9d2fdfdcc9b82
David Williams-Young <dbwy@uw.edu>
2021-02-01 16:38:02 -0800

    Revert "[CMAKE] Typo"
    This reverts commit 535679fc9b99b759e5ad444c37e7edaab08cefd2.

--- tests/cmake/subproject/scalapackpp_linker_test.cxx
+++ tests/cmake/subproject/scalapackpp_linker_test.cxx
@@ -3,7 +3,7 @@
 int main() {
   MPI_Init( NULL, NULL );
   {
-    auto grid = std::make_shared<const blacspp::Grid>(blacspp::Grid::square_grid( MPI_COMM_WORLD ));
+    std::shared_ptr<const Grid> grid = std::make_shared<const Grid>(blacspp::Grid::square_grid( MPI_COMM_WORLD ));
     scalapackpp::BlockCyclicDist2D dist( grid, 2, 2 );
   }
   MPI_Finalize();
